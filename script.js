50583703dcbcb739f73500d49d4826d64553f6c6
sdkwapis <sdkwapis@gmail.com>
2025-08-30 13:59:41 -0400

    it works!

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 function asUpSoDown(numbers) {
   const num = numbers.split("");
-  console.log(num);
- for (let i = 0; i <= num.length; i++) {
-  const currentValue = num[i];
+  //console.log(num);
   const newNum = [];
-   if (currentValue == 6) {
-     newNum.push(9);
-     console.log(newNum);
-   } else if (currentValue == 9) {
-     newNum.push(6)
-   } else {
-     newNum.push(0);
-   }
-   console.log(newNum);
- }} 
-asUpSoDown("690690")
+  for (let i = 0; i < num.length; i++) {
+    const currentValue = num[i];
+    if (currentValue == 6) {
+      newNum.push(9);
+    } else if (currentValue == 9) {
+      newNum.push(6);
+    } else {
+      newNum.push(0);
+    }
+  }
+  //console.log(newNum);
+  const numCompare = newNum.reverse();
+  const numCompareString = numCompare.join("");
+  //console.log(numCompare)
+  console.log(numCompareString);
+  console.log(numbers);
+  if (numCompareString == numbers) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
+asUpSoDown("690690");
+asUpSoDown("6090609");
